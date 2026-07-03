3a5d813760d869b5a3d4756f94b6e00b2106229a
Omar <omar_millan54@hotmail.com>
2026-07-03 02:47:25 -0700

    🐛

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,26 +1,25 @@
 const { Pool } = require("pg");
-require("dotenv").config(); // Esto carga las variables del archivo .env
+require("dotenv").config();
 
-// Configuramos el grupo de conexiones (Pool) usando las variables de entorno
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
+  ssl: require, // <-- Esto fuerza el SSL directo en el código si es necesario
 });
 
-// Probamos la conexión intentando conectar un cliente del pool
+// Probamos la conexión
 pool.connect((err, client, release) => {
   if (err) {
     console.error("Error al conectar a PostgreSQL: ", err.message);
   } else {
     console.log("Base de datos conectada con éxito.");
-    release(); // Devolvemos el cliente al pool de inmediato
+    release();
   }
 });
 
-// Exportamos la función query para que nuestros Modelos puedan usarla
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
